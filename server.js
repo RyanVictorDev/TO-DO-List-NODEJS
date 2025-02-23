4aa1a7d4bc877e1cff71ceca8d5d50da00e7b8f4
RyanVictorDev <vryan8294@gmail.com>
2025-02-23 12:47:15 -0300

    create: Create cors to project

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ import { DatabaseMemory } from './repository/database-memory.js';
 import { DatabasePostgres } from './repository/database-postgres.js';
 import { NoteModel } from './model/noteModel.js';
 import { noteRoutes } from './controller/noteController.js';
+import cors from '@fastify/cors'
 
 export const server = fastify();
 
 // const database = new DatabaseMemory();
 const database = new DatabasePostgres();
 const port = process.env.PORT || 3333
+
+server.register(cors, { 
+    origin: '*'
+});
 
 server.get('/', async (request, reply) => {
     return { hello: 'NAO ERA PROCE ESTAR AQUI KKKKKKKKKKKKKKK' };
